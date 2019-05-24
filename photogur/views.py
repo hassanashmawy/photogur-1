1285394484043c9e61c79c830a3ff3fb4447bfaf
cindychea <cindy_chea@hotmail.com>
2019-05-24 14:44:34 -0400

    completed photogur part 1

---
+++ photogur/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from photogur.models import Picture, Comment
+
+def root(request):
+    return HttpResponseRedirect('pictures')
+
+def pictures(request):
+    context = {
+        'pictures': Picture.objects.all(),
+    }
+    response = render(request, 'pictures.html', context) 
+    return HttpResponse(response)
