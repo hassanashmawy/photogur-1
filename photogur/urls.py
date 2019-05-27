bab139165f07adf78201a4d19a56442abe9aa5ea
cindychea <cindy_chea@hotmail.com>
2019-05-27 14:09:18 -0400

    added picture show page and comments

--- photogur/urls.py
+++ photogur/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from photogur.views import root, pictures
+from photogur.views import root, pictures, picture_show
 
 urlpatterns = [
     path('', root),
     path('admin/', admin.site.urls),
     path('pictures', pictures),
+    path('pictures/<int:id>', picture_show, name='picture_show'),
 ]
--- photogur/views.py
+++ photogur/views.py
@@ -11,3 +11,9 @@ def pictures(request):
     }
     response = render(request, 'pictures.html', context) 
     return HttpResponse(response)
+
+def picture_show(request, id):
+    picture = Picture.objects.get(pk=id)
+    context = {'picture': picture}
+    response = render(request, 'picture.html', context)
+    return HttpResponse(response)
