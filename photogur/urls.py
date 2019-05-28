0ddbb923e2d2d520d414b01a55822b33ad76eac5
cindychea <cindy_chea@hotmail.com>
2019-05-28 14:56:55 -0400

    added search and comment functionality to photogur

--- photogur/urls.py
+++ photogur/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from photogur.views import root, pictures, picture_show
+from photogur.views import root, pictures, picture_show, picture_search, create_comment
 
 urlpatterns = [
     path('', root),
     path('admin/', admin.site.urls),
     path('pictures', pictures, name='pictures'),
     path('pictures/<int:id>', picture_show, name='picture_show'),
+    path('search', picture_search, name='picture_search'),
+    path('comments/new', create_comment, name='create_comment'),
 ]
--- photogur/views.py
+++ photogur/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views.decorators.http import require_http_methods
 from photogur.models import Picture, Comment
 
 def root(request):
@@ -21,3 +23,24 @@ def picture_show(request, id):
     }
     response = render(request, 'picture.html', context)
     return HttpResponse(response)
+
+def picture_search(request):
+    query = request.GET['query']
+    search_results = Picture.objects.filter(artist=query)
+    context = {
+        'pictures': search_results,
+        'query': query,
+    }
+    response = render(request, 'picture_search.html', context)
+    return HttpResponse(response)
+
+@require_http_methods(['POST'])
+def create_comment(request):
+    user_name = request.POST['name']
+    user_message = request.POST['message']
+    picture_id = request.POST['picture']
+    picture = Picture.objects.get(id=picture_id)
+    # comment = Comment(name=user_name, picture=picture, message=user_message)
+    # comment.save()
+    Comment.objects.create(name=user_name, picture=picture, message=user_message)
+    return redirect('picture_show', id=picture_id)
